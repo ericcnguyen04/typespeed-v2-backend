96e4a686adb9d8c2c2394d857dbe323e1a67815e
ericcnguyen04 <ericc.win@gmail.com>
2024-01-05 14:49:07 -0500

    player schema

--- Models/Game.js
+++ Models/Game.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
 const PlayerSchmea = new mongoose.Schema({
-    
+    currentWordIndex : { // keeps track of what word user is on
+        type: Number,
+        default: 0
+    },
+    socketID: {type: String}, // has unique identifier 
+    isPartyLeader: {type: Boolean, default: false},
+    WPM: {type: Number, default: -1}
 })
