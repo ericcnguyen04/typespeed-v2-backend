3332c81f1302b3b1e7be76579c31ac9375754b4b
ericcnguyen04 <ericc.win@gmail.com>
2024-01-15 19:38:08 -0500

    more debug

--- client/src/history.js
+++ client/src/history.js
@@ -1,4 +1,4 @@
-import { createBrowserHistory } from 'history';
+import { createBrowserHistory } from './history';
 // create history object
 const history = createBrowserHistory();
 export default history
