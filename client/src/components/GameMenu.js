41d29952c802971eaba4da62c50bd890943d5410
ericcnguyen04 <ericc.win@gmail.com>
2024-01-15 19:32:40 -0500

    another join btn

--- client/src/components/GameMenu.js
+++ client/src/components/GameMenu.js
@@ -7,8 +7,9 @@ const GameMenu = props => {
         <div className='text-center'>
             <h1>Welcome to TypeSpeed</h1>
             <button type="button" onClick={()=> history.push('/game/create')} className='btn btn-primary btn-lg mr-3'> Create Game </button>
+            <button type="button" onClick={()=> history.push('/game/join')} className='btn btn-primary btn-lg'> Join Game </button>
 
-            
+
 
         </div>
     )
