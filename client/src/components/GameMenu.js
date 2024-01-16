fdf78db8a078ddaac1c818a7c808fbf37ec60240
ericcnguyen04 <ericc.win@gmail.com>
2024-01-15 19:29:44 -0500

    intro page

--- client/src/components/GameMenu.js
+++ client/src/components/GameMenu.js
@@ -4,7 +4,11 @@ import {useHistory } from 'react-router-dom'
 const GameMenu = props => {
     let history = useHistory();
     return (
-        
+        <div className='text-center'>
+            <h1>Welcome to TypeSpeed</h1>
+            <button type="button" onClick={()=> history.push('/game/create')}></button>
+            
+        </div>
     )
 }
 
